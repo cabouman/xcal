c461effcc73c372cad4d43a68fc25deaf60a87c4
DamonLee5 <liwr5damon@gmail.com>
2023-02-27 11:16:30 -0500

    Update pairwise ICD with 2 loop

--- xspec/xspec.py
+++ xspec/xspec.py
@@ -9,7 +9,7 @@ Date: Dec 1st, 2022.
 
 
 import numpy as np
-import matplotlib.pyplot as plt
+import itertools
 
 from xspec._utils import get_wavelength, binwised_spec_cali_cost, huber_func
 
@@ -147,11 +147,7 @@ class Snap:
         self.l_star = l_star
         self.max_iter = max_iter
         self.threshold = threshold
-        self.mbi = 0  # Max beta index
         self.cost_list = []
-
-    def _set_mbi(self, mbi):
-        self.mbi = mbi
 
     def cost(self):
         """Return current value of MAP cost function.
@@ -204,36 +200,35 @@ class Snap:
         self.weight = weight
         self.cost_list.append(self.cost())
         for k in range(self.max_iter):
-            permuted_ind = np.random.permutation(n)
-            permuted_ind = permuted_ind[permuted_ind != self.mbi]
             tol_update = 0
-            for i in permuted_ind:
+            pairs = np.array(list(itertools.combinations(n, 2)))
+            for pair_ind in pairs:
+                i = pair_ind[0]
+                j = pair_ind[1]
                 omega_tmp = Omega[i]
-                omega_mbi = Omega[self.mbi]
-                theta_1 = -(e * weight).T @ (FD[:, i:i + 1] - FD[:, self.mbi:self.mbi + 1]) / m - l * (
+                omega_mbi = Omega[j]
+                theta_1 = -(e * weight).T @ (FD[:, i:i + 1] - FD[:, j:j + 1]) / m - l * (
                             omega_tmp - omega_mbi)
-                theta_2 = ((FD[:, i:i + 1] - FD[:, self.mbi:self.mbi + 1]) * weight).T @ (
-                        FD[:, i:i + 1] - FD[:, self.mbi:self.mbi + 1]) / m - 2 * l
+                theta_2 = ((FD[:, i:i + 1] - FD[:, j:j + 1]) * weight).T @ (
+                        FD[:, i:i + 1] - FD[:, j:j + 1]) / m - 2 * l
                 if theta_2 < 0:
                     update = -np.sign(theta_1)
                 else:
                     update = -theta_1 / theta_2
                 Omega[i] = np.clip(omega_tmp + update, 0, omega_tmp + omega_mbi)
-                Omega[self.mbi] = np.clip(omega_mbi - update, 0, omega_tmp + omega_mbi)
+                Omega[j] = np.clip(omega_mbi - update, 0, omega_tmp + omega_mbi)
                 tol_update += np.abs(Omega[i] - omega_tmp)
-                e = e - (FD[:, i:i + 1] - FD[:, self.mbi:self.mbi + 1]) * (Omega[i] - omega_tmp)
+                e = e - (FD[:, i:i + 1] - FD[:, j:j + 1]) * (Omega[i] - omega_tmp)
             self.Omega = Omega
             self.e = e
             self.weight = weight
-            self.mbi = np.argmax(Omega)
-            if tol_update > self.threshold / 10:
+            if tol_update > self.threshold:
                 self.cost_list.append(self.cost())
 
             if tol_update < self.threshold:
                 print('Stop at iteration:', k, '  Total update:', tol_update)
                 break
 
-        print('mbi, omega_mbi:', self.mbi, Omega[self.mbi])
         print('Omega', Omega)
 
         return Omega
@@ -343,10 +338,6 @@ def dictSE(signal, energies, forward_mat, spec_dict, sparsity, optimizor, signal
 
     selected_spec_mask = np.zeros(spec_dict.shape[1], dtype=bool)
 
-    # wavelengths = get_wavelength(energies)
-    # wnum = 2 * np.pi / wavelengths
-    # forward_mat = np.exp(-2 * wnum * beta_projs.transpose((0, 2, 3, 4, 1))).reshape((-1, len(energies)))
-
     if verbose > 0:
         print(forward_mat.shape)
         print(np.diag(signal_weight).shape)
@@ -432,8 +423,6 @@ def dictSE(signal, energies, forward_mat, spec_dict, sparsity, optimizor, signal
 
         print('e:', np.sqrt(np.mean(e ** 2 * signal_weight)))
         print('cost:', cost)
-        optimizor._set_mbi(np.argmax(omega[S, 0].flatten()))
-        #optimizor.set_mbi(len(S))
         estimated_spec = spec_dict @ omega
         estimated_spec_list.append(estimated_spec)
 
@@ -478,197 +467,5 @@ def cal_fw_mat(solid_vol_masks, obj_dicts,energies, fw_projector):
     return fw_mat
 
 
-# def binwised_spec_cali(signal, energies, x_init, h_init, beta_projs,
-#                        B, h_min, energies_weight=None,beta=1, c=0.01, tol=1e-6,max_iter=200, stop_iter=1e-11,
-#                        s_update_threshold=1,j_update_threshold=None):
-#     """
-#
-#     Parameters
-#     ----------
-#     signal
-#     energies
-#     x_init
-#     h_init
-#     beta_projs
-#     B
-#     h_min
-#     beta
-#     c
-#     tol
-#
-#     Returns
-#     -------
-#
-#     """
-#     if energies_weight is None:
-#         energies_weight = np.ones(energies.shape)
-#     W = np.diag(energies_weight)
-#     signal = signal.reshape((-1, 1))
-#     Ne = len(x_init)
-#     Nc = len(h_init)
-#     wavelengths = get_wavelength(energies)
-#     wnum = 2 * np.pi / wavelengths
-#     F = np.exp(-2 * wnum * beta_projs.transpose((0, 2, 3, 4, 1))).reshape((-1, len(energies)))
-#     m, n = np.shape(F)
-#
-#     x = x_init.copy().reshape((-1,1))
-#
-#     if j_update_threshold is None:
-#         j_update_threshold = 1/n
-#     h = h_init.copy().reshape((-1,1))
-#     e = signal - F@W @ (x + B @ h)
-#     iteration=0
-#     legend=[]
-#     fig,ax = plt.subplots(figsize=(24,12))
-#     cost_x_list=[]
-#     cost_rho_list=[]
-#     sp_list = []
-#
-#     while np.linalg.norm(e) > tol and iteration<max_iter:
-#         print()
-#         total_abs_update = 0
-#         total_update = 0
-#         if iteration % int(max_iter*0.1)==0:
-#             ax.plot(energies, x+B@h)
-#             legend.append('iter:%d'%iteration)
-#         iteration+=1
-#         # Find index to perform direct substitution
-#
-#         if len(sp_list) ==0:
-#             j = np.argmax(x[:,0]*energies_weight)
-#         else:
-#             mask = np.ones(energies_weight.shape)
-#             mask[j] = 0
-#             j = np.argmax(np.abs((x[:,0]-pre_x[:,0]))*energies_weight*mask)
-#         pre_x = x.copy()
-#         permuted_ind = np.random.permutation(Ne)
-#         # Update x serially
-#         for s in permuted_ind:
-#             if s == j:
-#                 continue
-#             delta_1 = []
-#             delta_2 = []
-#             dsj = np.zeros(len(x))
-#             dsj[s]=1/energies_weight[s]
-#             dsj[j]=-1/energies_weight[j]
-#             if s > 0:
-#                 we = (energies_weight[s]**2*np.abs(energies[s]-energies[s-1]))
-#                 dd = (x[s] - x[s - 1])/np.abs(energies[s]-energies[s-1])
-#                 dws = dsj[s]-dsj[s-1]
-#                 if np.abs(dd) <1e-8:
-#                     bs1 = beta / 2/we
-#                 else:
-#                     bs1 = beta * np.clip(dd, -c, c) / (2 * dd)/we
-#                 delta_1.append(bs1*dd*dws)
-#                 delta_2.append(bs1*dws**2)
-#
-#             if s < Ne - 1:
-#                 we = (energies_weight[s]**2*np.abs(energies[s]-energies[s + 1]))
-#                 dd = (x[s] - x[s + 1])/np.abs(energies[s]-energies[s + 1])
-#                 dws = dsj[s]-dsj[s+1]
-#                 if np.abs(dd) <1e-8:
-#                     bs2 = beta / 2/we
-#                 else:
-#                     bs2 = beta * np.clip(dd, -c, c) / (2 * dd)/we
-#                 delta_1.append(bs2*dd*dws)
-#                 delta_2.append(bs2*dws**2)
-#
-#             if j > 0:
-#                 we = (energies_weight[j]**2*np.abs(energies[j] - energies[j - 1]))
-#                 dd = (x[j - 1] - x[j])/np.abs(energies[j] - energies[j - 1])
-#                 dws = dsj[j-1]-dsj[j]
-#                 if np.abs(dd) <1e-8:
-#                     bj1 = beta / 2 /we
-#                 else:
-#                     bj1 = beta * np.clip(dd, -c, c) / (2 * dd)/we
-#                 delta_1.append(bj1*dd*dws)
-#                 delta_2.append(bj1*dws**2)
-#
-#             if j < Ne - 1:
-#                 we = (energies_weight[j]**2*np.abs(energies[j]-energies[j + 1]))
-#                 dd = (x[j + 1] - x[j])/np.abs(energies[j] - energies[j + 1])
-#                 dws = dsj[j+1]-dsj[j]
-#                 if np.abs(dd) <1e-8:
-#                     bj2 = beta / 2 /we
-#                 else:
-#                     bj2 = beta * np.clip(dd, -c, c) / (2 * dd)/we
-#                 delta_1.append(bj2*dd*dws)
-#                 delta_2.append(bj2*dws**2)
-#             Fsj = F[:, s:s + 1] - F[:, j:j + 1]
-#             theta_x_1 = -e.T @ Fsj / m + 2 * np.sum(np.array(delta_1))
-#             theta_x_2 = Fsj.T @ Fsj / m + 2 * np.sum(np.array(delta_2))
-#             Tmin = max(-x[j]*energies_weight[j]*j_update_threshold,-x[s]*energies_weight[s]*s_update_threshold)
-#             Tmax = min(x[s]*energies_weight[s]*s_update_threshold,x[j]*energies_weight[j]*j_update_threshold)
-#
-#
-#             update_alpha = np.clip(-theta_x_1 / theta_x_2, Tmin, Tmax).reshape((1,))
-#             total_abs_update +=np.abs(update_alpha)
-#             total_update +=update_alpha
-#             x[s] += update_alpha/energies_weight[s]
-#             x[j] -= update_alpha/energies_weight[j]
-#             e -= Fsj * update_alpha
-#
-#         print('A1. Iter:', iteration,'Max index',j, 'Updated Err:', np.linalg.norm(e),
-#               'True Err:',np.linalg.norm(signal - F@W @ (x + B @ h)),'W (x+Bh)=',np.sum(W@(x+B@h)))
-#         cost_x,cost_rho = binwised_spec_cali_cost(signal,x,h,F,W,B,beta,c,energies)
-#         cost_x_list.append(cost_x)
-#         cost_rho_list.append(cost_rho)
-#         print('A. iter:',iteration,'cost_x:',cost_x,'cost_rho:',cost_rho)
-#         for s in range(Nc):
-#             delta_1 = []
-#             delta_2 = []
-#             if s == j:
-#                 continue
-#             dsj = np.zeros(len(x))
-#             dsj[s]=1/energies_weight[s]
-#             dsj[j]=1/energies_weight[j]
-#             if j > 0:
-#                 we = (energies_weight[j]**2*np.abs(energies[j] - energies[j - 1]))
-#                 dd = (x[j - 1] - x[j])/np.abs(energies[j] - energies[j - 1])
-#                 dws = dsj[j-1]-dsj[j]
-#                 if np.abs(dd) <1e-8:
-#                     bj1 = beta / 2/we
-#                 else:
-#                     bj1 = beta * np.clip(dd, -c, c) / (2 * dd)/we
-#                 delta_1.append(bj1*dd*dws)
-#                 delta_2.append(bj1*dws**2)
-#             if j < Ne - 1:
-#                 we = (energies_weight[j]**2*np.abs(energies[j]-energies[j + 1]))
-#                 dd = (x[j + 1] - x[j])/np.abs(energies[j] - energies[j + 1])
-#                 dws = dsj[j+1]-dsj[j]
-#                 if np.abs(dd) <1e-8:
-#                     bj2 = beta / 2/we
-#                 else:
-#                     bj2 = beta * np.clip(dd, -c, c) / (2 * dd)/we
-#                 delta_1.append(bj2*dd*dws)
-#                 delta_2.append(bj2*dws**2)
-#             FB = F@ W @ B[:, s:s + 1]
-#             FBj = FB - F[:, j:j + 1]
-#             theta_h_1 = -e.T @ FBj / m + 2 * np.sum( np.array(delta_1))
-#             theta_h_2 = FBj.T @ FBj / m + 2 * np.sum(np.array(delta_2))
-#             Tmin = max(-x[j]*energies_weight[j]*j_update_threshold,h_min[s] - h[s])
-#             Tmax = x[j]*energies_weight[j]*j_update_threshold
-#             update_h = np.clip(-theta_h_1 / theta_h_2, Tmin, Tmax).reshape((1,))
-#             h[s] += update_h
-#             x[j] -= update_h/energies_weight[j]
-#             total_abs_update +=np.abs(update_h)
-#             total_update +=update_h
-#
-#             e -= FBj * update_h
-#         cost_x,cost_rho_s = binwised_spec_cali_cost(signal,x,h,F,W,B,beta,c,energies)
-#         cost_x_list.append(cost_x)
-#         cost_rho_list.append(cost_rho_s)
-#         sp_list.append(x + B @ h)
-#         print('B1. Iter:', iteration,'Max index',j, 'Updated Err:', np.linalg.norm(e),
-#               'True Err:',np.linalg.norm(signal - F@W @ (x + B @ h)),'W (x+Bh)=',np.sum(W@(x+B@h)))
-#         print('B. iter:',iteration,'cost_x:',cost_x,'cost_rho:',cost_rho)
-#         print('Total Update:',total_update, 'Total abs update:',total_abs_update )
-#         if total_abs_update < stop_iter:
-#             break
-#     ax.plot(energies, x+B@h)
-#     legend.append('iter:%d'%iteration)
-#     plt.legend(legend)
-#
-#     return x, h, cost_x_list, cost_rho_list, sp_list
 
 
