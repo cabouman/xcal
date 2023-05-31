0f78ab98fe335518e9f67b9bbd36e1ffba7161fc
DamonLee5 <liwr5damon@gmail.com>
2023-05-30 23:41:04 -0400

    Modify input signal and forward matrix for different geometry setting in each set.

--- xspec/xspec.py
+++ xspec/xspec.py
@@ -551,7 +551,7 @@ def dictSE(signal, energies, forward_mat, spec_dict, sparsity, optimizor, signal
 
     S = []
     signal = np.concatenate([sig.reshape((-1, 1)) for sig in signal])
-    forward_mat = np.concatenate([fwm.reshape((-1, forward_mat.shape[-1])) for fwm in forward_mat]).reshape((-1, 1))
+    forward_mat = np.concatenate([fwm.reshape((-1, forward_mat.shape[-1])) for fwm in forward_mat])
 
     if signal_weight is None:
         signal_weight = np.ones(signal.shape)
