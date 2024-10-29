86074b2219254b23605deb6a986e3d85e5dd1776
Wenrui Li <li3120@login02.negishi.rcac.purdue.edu>
2024-10-29 15:06:20 -0400

    Add tutorial

--- xspec/__init__.py
+++ xspec/__init__.py
@@ -1,4 +1,5 @@
 __version__ = '0.1.0'
 from .defs import *
 from .estimate import Estimate, calc_forward_matrix
-__all__ = ['Estimate', 'Material','calc_forward_matrix']
+from .tools import get_filter_response, get_scintillator_response
+__all__ = ['Estimate', 'Material','calc_forward_matrix','get_filter_response','get_scintillator_response']
